315de467bc479c51d7ba6c968a12e4f7bd4928e3
Eric Beda <ericbeda@gmail.com>
2018-12-16 04:35:03 +0300

    updated top nav view, removed unnecessary items

--- application/modules/Project/views/top_nav.php
+++ application/modules/Project/views/top_nav.php
@@ -96,114 +96,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <ul class="navbar-nav">
                 
 
-                <li class="nav-item dropdown">
-                    <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                        <i class="icon-bubbles4"></i>
-                        <span class="d-md-none ml-2">Messages</span>
-                        <span class="badge badge-mark border-pink-400 ml-auto ml-md-0"></span>
-                    </a>
-                    
-                    <div class="dropdown-menu dropdown-menu-right dropdown-content wmin-md-350">
-                        <div class="dropdown-content-header">
-                            <span class="font-weight-semibold">Messages</span>
-                            <a href="#" class="text-default"><i class="icon-compose"></i></a>
-                        </div>
-
-                        <div class="dropdown-content-body dropdown-scrollable">
-                            <ul class="media-list">
-                                <li class="media">
-                                    <div class="mr-3 position-relative">
-                                        <img src="<?php echo base_url(); ?>vendors/limitless/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-
-                                    <div class="media-body">
-                                        <div class="media-title">
-                                            <a href="#">
-                                                <span class="font-weight-semibold">James Alexander</span>
-                                                <span class="text-muted float-right font-size-sm">04:58</span>
-                                            </a>
-                                        </div>
-
-                                        <span class="text-muted">who knows, maybe that would be the best thing for me...</span>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3 position-relative">
-                                        <img src="<?php echo base_url(); ?>vendors/limitless/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-
-                                    <div class="media-body">
-                                        <div class="media-title">
-                                            <a href="#">
-                                                <span class="font-weight-semibold">Margo Baker</span>
-                                                <span class="text-muted float-right font-size-sm">12:16</span>
-                                            </a>
-                                        </div>
-
-                                        <span class="text-muted">That was something he was unable to do because...</span>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="<?php echo base_url(); ?>vendors/limitless/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="media-title">
-                                            <a href="#">
-                                                <span class="font-weight-semibold">Jeremy Victorino</span>
-                                                <span class="text-muted float-right font-size-sm">22:48</span>
-                                            </a>
-                                        </div>
-
-                                        <span class="text-muted">But that would be extremely strained and suspicious...</span>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="<?php echo base_url(); ?>vendors/limitless/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="media-title">
-                                            <a href="#">
-                                                <span class="font-weight-semibold">Beatrix Diaz</span>
-                                                <span class="text-muted float-right font-size-sm">Tue</span>
-                                            </a>
-                                        </div>
-
-                                        <span class="text-muted">What a strenuous career it is that I've chosen...</span>
-                                    </div>
-                                </li>
-
-                                <li class="media">
-                                    <div class="mr-3">
-                                        <img src="<?php echo base_url(); ?>vendors/limitless/global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="media-title">
-                                            <a href="#">
-                                                <span class="font-weight-semibold">Richard Vango</span>
-                                                <span class="text-muted float-right font-size-sm">Mon</span>
-                                            </a>
-                                        </div>
-                                        
-                                        <span class="text-muted">Other travelling salesmen live a life of luxury...</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="dropdown-content-footer bg-light">
-                            <a href="#" class="text-grey mr-auto">All messages</a>
-                            <div>
-                                <a href="#" class="text-grey" data-popup="tooltip" title="Mark all as read"><i class="icon-radio-unchecked"></i></a>
-                                <a href="#" class="text-grey ml-2" data-popup="tooltip" title="Settings"><i class="icon-cog3"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
 
                 <li class="nav-item dropdown dropdown-user">
                     <a href="#" class="navbar-nav-link dropdown-toggle" data-toggle="dropdown">
@@ -213,8 +105,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
                     <div class="dropdown-menu dropdown-menu-right">
                         <a href="#" class="dropdown-item"><i class="icon-user-plus"></i> My profile</a>
-                        <a href="#" class="dropdown-item"><i class="icon-coins"></i> My balance</a>
-                        <a href="#" class="dropdown-item"><i class="icon-comment-discussion"></i> Messages <span class="badge badge-pill bg-indigo-400 ml-auto">58</span></a>
                         <div class="dropdown-divider"></div>
                         <a href="#" class="dropdown-item"><i class="icon-cog5"></i> Account settings</a>
                         <a href="<?php echo base_url('auth/logout'); ?>" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
